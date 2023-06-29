5408a84d269b13b00c11ae3e44feb9b1e3661913
alzC <69784004+alzC@users.noreply.github.com>
2023-06-29 21:07:06 +0200

    Game model done
    configuration et typage du model de jeu

---
+++ src/machine/GameMachine.ts
@@ -0,0 +1,77 @@
+import { createMachine } from "xstate";
+import { createModel } from "xstate/lib/model";
+import { GridState, Player, PlayerColor } from "../types";
+enum GameStates {
+    LOBBY = 'LOBBY',
+    PLAY = 'PLAY',
+    VICTORY = 'VICTORY',
+    DRAW = 'DRAW'
+}
+export const GameModel = createModel({
+    players: [] as Player[],
+    currentPlayer: null as null | Player['id'],
+    rowLength: 4,
+    grid: [
+            ["E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "E"],
+            ["E", "E", "E", "E", "E", "E", "E"]
+        
+    ] as GridState
+},{
+    events: {
+        join: (playerId: Player['id'], name: Player['name']) => ({ playerId, name }),
+        leave: (playerId: Player['id']) => ({ playerId }),
+        chooseColor: (playerId: Player['id'], color: PlayerColor) => ({ playerId, color }),
+        start: (playerId: Player['id']) => ({ playerId }),
+        dropToken: (playerId: Player['id'], x: number) => ({ playerId, x }),
+        restart: (playerId: Player['id']) => ({ playerId }),
+    }
+})
+
+
+export const GameMachine = createMachine({
+    id: 'game',
+    initial: GameStates.LOBBY,
+    states: {
+        [GameStates.LOBBY]: {
+            on: {
+                join: {
+                    target: GameStates.LOBBY
+                },
+                leave: {
+                    target: GameStates.LOBBY
+                },
+                chooseColor: {
+                    target: GameStates.LOBBY
+                },
+                start: {
+                    target: GameStates.PLAY
+                }
+            },
+        },
+        [GameStates.PLAY]: {
+            on: {
+                dropToken: {
+                target: '???'
+                },
+            }
+        },
+         [GameStates.VICTORY]: {
+            on: {
+                restart: {
+                target: GameStates.LOBBY
+                },
+            }
+        },
+          [GameStates.DRAW]: {
+            on: {
+                restart: {
+                target: GameStates.LOBBY
+                },
+            }
+        }
+    }
+})
---
+++ src/types.ts
@@ -0,0 +1,19 @@
+import { ContextFrom,EventFrom } from 'xstate'
+import { GameModel } from "./machine/GameMachine"
+
+export enum PlayerColor {
+    RED = 'red',
+    YELLOW= 'yellow'
+}
+
+export type Player = {
+    id: string,
+    name: string,
+    color?: PlayerColor
+}
+
+export type CellEmpty = 'E'
+export type CellState = PlayerColor.RED | PlayerColor.YELLOW | CellEmpty
+export type GridState = CellState[][]
+export type GameContext = ContextFrom<typeof GameModel>
+export type GameEvents = EventFrom<typeof GameModel>
